e757c262e7aa984a23c17baf407884c1fa8b70ce
Aaron G <ag505181@gmail.com>
2019-04-17 11:09:09 -0700

    find the longest work in string

---
+++ javaScript/longerWord.js
@@ -0,0 +1,23 @@
+//Find the longest string from an given array of strings
+
+function longest_string(str) {
+  //split array up to find the longest string
+  new_str = str.split(' ')
+  //will hold the longest string length
+  let longestWord = 0
+
+//create a loop to check each string length
+
+  for( let i = 0; i < new_str.length; i++){
+    //need to hold the longest word until replace
+    if(new_str[i].length > longestWord){
+      //true
+      longestWord = new_str[i].length
+      theword = new_str[i]
+    }
+  }
+  console.log(new_str, longestWord, theword);
+  
+}
+
+longest_string("The quick brown fox jumped over the lazy dog")
