4c814d296330700156ba822f6e8efb00bd504e1f
Aaron <ag505181@gmail.com>
2019-05-18 10:59:38 -0700

    animatable

--- expo-app/confusion/components/DishdetailComponent.js
+++ expo-app/confusion/components/DishdetailComponent.js
@@ -1,40 +1,51 @@
-import React, { Component } from "react";
-import { Text, View } from "react-native";
-import { Card } from "react-native-elements";
-import { DISHES } from "../shared/dishes";
-
+import React, { Component } from 'react';
+import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet } from 'react-native';
+import { Card, Icon, Input, Rating } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import { postFavorite, postComment } from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
+ 
+const mapStateToProps = state => {
+    return {
+        dishes: state.dishes,
+        comments: state.comments,
+      favorites: state.favorites
+    }
+};
+ 
+const mapDispatchToProps = dispatch => ({
+    postFavorite: (dishId) => dispatch(postFavorite(dishId)),
+    postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, 
+comment))
+});
+ 
 function RenderDish(props) {
-  const dish = props.dish;
-  if (dish != null) {
-    return (
-      <Card
-        featuredTitle={dish.name}
-        image={require("./images/uthappizza.png")}
-      >
-        <Text style={{ margin: 10 }}>{dish.description}</Text>
-      </Card>
-    );
-  } else {
-    return <View />;
-  }
-}
-
-class DishDetail extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
-
-  static navigationOptions = {
-    title: "Dish Details"
-  };
-
-  render() {
-    const dishId = this.props.navigation.getParam("dishId", "");
-    return <RenderDish dish={this.state.dishes[+dishId]} />;
-  }
-}
-
-export default DishDetail;
+    const dish = props.dish;
+    
+    if (dish != null) {
+        return(
+            <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+                <Card
+                    featuredTitle={dish.name}
+                    image={{uri: baseUrl + dish.image}}>
+                    <Text style={{margin: 10}}>
+                        {dish.description}
+                    </Text>
+                    <View style={styles.cardRow}>
+                        <Icon
+                            raised
+                            reverse
+                            name={ props.favorite ? 'heart' : 'heart-o'}
+                            type='font-awesome'
+                            color='#f50'
+                            onPress={() => props.favorite ? console.log('Already favorite') : 
+props.onPress()}
+                            />
+                        <Icon
+                            raised
+                            reverse
+                            name={'pencil'}
+                            type='font-awesome'
+                            color='#512DA8'
+                            style={styles.cardItem}
